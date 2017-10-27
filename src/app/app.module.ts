ea8bdcdef791df07387a1b51fc99eb993959592d
Uday Kant <udaydeveloper@gmail.com>
2017-10-27 19:05:29 +0530

    First Commit of Angular Static

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule, RouterLinkActive } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FriendsComponent } from './friends/friends.component';
+import { FamilyComponent } from './family/family.component';
+import { PhotosComponent } from './photos/photos.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    HeaderComponent,
+    FooterComponent,
+    FriendsComponent,
+    FamilyComponent,
+    PhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: PageComponent },
+      { path: 'friends', component: FriendsComponent },
+      { path: 'family', component: FamilyComponent },
+      { path: 'photos', component: PhotosComponent },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
